0f457ef0302748f539ba4fb6a74671005dafdb0f
Matt Vella <mcvella@gmail.com>
2025-02-26 18:41:04 -0500

    add sqlite3 dep via apt

--- run.sh
+++ run.sh
@@ -55,7 +55,7 @@ if [ -f .installed ]
     apt install python3.10-venv -y
     python3 -m venv viam-env
     source viam-env/bin/activate
-    apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev libcairo2-dev libxt-dev libgirepository1.0-dev -y
+    apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev libcairo2-dev libxt-dev libgirepository1.0-dev sqlite3 -y
     pip3 install --upgrade -r requirements.txt
     if [ $? -eq 0 ]
       then
