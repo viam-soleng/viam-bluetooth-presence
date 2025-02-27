6137c18d68bc29f3763b63301ae5e14e7935595a
Matt Vella <mcvella@gmail.com>
2025-02-27 12:21:59 -0500

    update viam-sdk version

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ async def main():
     """This function creates and starts a new module, after adding all desired resources.
     Resources must be pre-registered. For an example, see the `__init__.py` file.
     """
-    Registry.register_resource_creator(Sensor.SUBTYPE, bluetooth.MODEL, ResourceCreatorRegistration(bluetooth.new, bluetooth.validate))
+    Registry.register_resource_creator(Sensor.API, bluetooth.MODEL, ResourceCreatorRegistration(bluetooth.new, bluetooth.validate))
 
     module = Module.from_args()
-    module.add_model_from_registry(Sensor.SUBTYPE, bluetooth.MODEL)
+    module.add_model_from_registry(Sensor.API, bluetooth.MODEL)
     await module.start()
 
 if __name__ == "__main__":
