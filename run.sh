3a705dcbb00558464c59cca19ff96737087714d4
hunter-volkman <huntervolkman@protonmail.com>
2025-03-17 11:03:36 -0400

    Fix PyGObject build on aarch64

--- run.sh
+++ run.sh
@@ -4,29 +4,34 @@ cd "$(dirname "$0")"
 
 # Function to compare version numbers
 version_compare() {
-  if [[ $1 == $2 ]]; then
+  if [[ $1 == $2 ]] 
+  then
       return 0
   fi
   local IFS=.
   local i ver1=($1) ver2=($2)
-  for (( i=${#ver1[@]}; i<${#ver2[@]}; i++ )); do
+  for (( i=${#ver1[@]}; i<${#ver2[@]}; i++ )) 
+  do
       ver1[i]=0
   done
-  for (( i=0; i<${#ver1[@]}; i++ )); do
-      if [[ -z ${ver2[i]} ]]; then
+  for (( i=0; i<${#ver1[@]}; i++ ))
+  do
+      if [[ -z ${ver2[i]} ]] 
+      then
           ver2[i]=0
       fi
-      if ((10#${ver1[i]} > 10#${ver2[i]})); then
+      if ((10#${ver1[i]} > 10#${ver2[i]})) 
+      then
           return 1
       fi
-      if ((10#${ver1[i]} < 10#${ver2[i]})); then
+      if ((10#${ver1[i]} < 10#${ver2[i]})) 
+      then
           return 2
       fi
   done
   return 0
 }
 
-# If already installed, just activate the virtual environment.
 if [ -f .installed ]; then
     source viam-env/bin/activate
 else
@@ -53,7 +58,7 @@ else
               /usr/lib/aarch64-linux-gnu/pkgconfig/girepository-2.0.pc
     fi
 
-    # Export environment variables to help pkg-config and PyGObject find the necessary files.
+    # Export environment variables to help pkg-config and PyGObject find the necessary files
     export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
     export GI_TYPELIB_PATH=/usr/lib/aarch64-linux-gnu/girepository-1.0:$GI_TYPELIB_PATH
 
@@ -61,11 +66,14 @@ else
     pip_version=$(python3 -m pip --version | awk '{print $2}')
     echo "Detected pip version: $pip_version"
     base_command="python3 -m pip install --user virtualenv"
-    if version_compare "$pip_version" "23.0"; then
-        if [[ $? -eq 1 ]] || [[ $? -eq 0 ]]; then
+    if version_compare "$pip_version" "23.0" 
+    then
+        if [[ $? -eq 1 ]] || [[ $? -eq 0 ]]
+        then
             base_command="$base_command --break-system-packages"
         fi
     fi
+
     $base_command
 
     # Create and activate the virtual environment
@@ -73,7 +81,7 @@ else
     source viam-env/bin/activate
     pip3 install --upgrade pip
 
-    # Force reinstall PyGObject at a version compatible with your system (3.42.1)
+    # Force reinstall PyGObject at a version compatible with system
     pip3 install --prefer-binary --force-reinstall "PyGObject==3.42.1"
 
     # Install the remaining Python dependencies
